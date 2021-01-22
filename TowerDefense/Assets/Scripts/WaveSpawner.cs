72f6542ec97e1e66fcaf6cce48f4d98fc27960a7
Chris Shaffer <clshaffer91@yahoo.com>
2021-01-22 02:35:46 -0500

    Removed dependency on setting PreFabs for WaveSpawner in the Unity UI.  Implemented some plumbing for multiple spawning algorithms

--- TowerDefense/Assets/Scripts/WaveSpawner.cs
+++ TowerDefense/Assets/Scripts/WaveSpawner.cs
@@ -1,25 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace TowerDefense.Scripts
 {
+    //TODO: Instantiate this on some kind of GameLoad().  
+    //This class is becoming starting to blend with a GameManager - should break up responsibilities
     public class WaveSpawner : MonoBehaviour
     {
-        //how to set these that's not thru unity
-        public Transform enemyPrefab;
-        public Transform spawnPoint;
-        public Text waveCountdownText;
+        [Header("Prefabs")]
+        private Transform enemyPrefab;
+        private Transform spawnPoint;
+        private Text waveCountdownText;
 
-        //readonly?
+        //load these from a data source
+        [Header("Spawning Properties")]
         private float waveStaggerTime = 5f;    //TODO: what if it takes longer to spawn a wave than the waveStaggerTime? -> non-issue
         private float enemyStaggerTime = .3f;
         private float initialCountdown = 2f;
         private float waveCountdown = 10f;
 
+        private SpawningAlgorithm algorithm = SpawningAlgorithm.LinearWaveNumber;
         private int waveNumber = 1;
 
-        private bool done = false;
+        private enum SpawningAlgorithm
+        {
+            LinearWaveNumber,
+            Exponential
+        }
+
+        private void Awake()
+        {
+            enemyPrefab = (Transform)Resources.Load("PreFabs/Enemy", typeof(Transform));
+            spawnPoint = (Transform)Resources.Load("PreFabs/Start", typeof(Transform));
+            Instantiate(spawnPoint);
+            var waveCountdownPrefab = (Text)Resources.Load("PreFabs/WaveCountdown", typeof(Text));
+            if (waveCountdownPrefab != null)
+            {
+                GameObject canvas = new GameObject();
+                canvas.AddComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
+                canvas.name = "Canvas";
+                waveCountdownText = Instantiate(waveCountdownPrefab);
+                waveCountdownText.gameObject.transform.parent = canvas.transform;
+            }
+        }
 
         // Start is called before the first frame update
         void Start()
@@ -48,18 +73,28 @@ namespace TowerDefense.Scripts
         private void RoundStart()
         {
             //TODO: Understand logging
-            Debug.Log("Wave Incoming!");
+            //Debug.Log("Wave Incoming!"); This log is really noisey until we implement an actual round system
         }
 
         private IEnumerator SpawnWave()
         {
-            Debug.Log("Spawning wave with algo x");  //TODO: Implement different Spawning algos
+            Debug.Log("Spawning wave with algorithm: " + algorithm.ToString());  //TODO: Implement different Spawning algos
 
-            //abstract algo away - interface it
-            for (int i = 0; i <= waveNumber; i++)
+            switch (algorithm)
             {
-                SpawnEnemy();
-                yield return new WaitForSeconds(enemyStaggerTime);
+                case SpawningAlgorithm.LinearWaveNumber:
+                    {
+                        for (int i = 0; i <= waveNumber; i++)
+                        {
+                            SpawnEnemy();
+                            yield return new WaitForSeconds(enemyStaggerTime);
+                        }
+                        break;
+                    }
+                case SpawningAlgorithm.Exponential:
+                    throw new NotImplementedException();
+                default:
+                    throw new InvalidOperationException();
             }
         }
 
